c149ce8cf4ceafb8ae1b8114c269418571ec6f26
Dragesia <brkgzlsy@gmail.com>
2022-06-28 01:25:16 +0300

    finished

--- rps.js
+++ rps.js
@@ -30,31 +30,107 @@ const rules = ["rock", "scissors", "paper"];
             }
         }
 
-        function game() {
-            let userScore = 0;
-            let pcScore = 0;
+        let userScore = 0;
+        let pcScore = 0;
 
-            
-            let userPlay = prompt("Rock, paper or scissors?").toLowerCase();
-            let result = playRound(userPlay, computerPlay());
-            console.log(`Round ${i+1}: `, result);
+        const playerSb = document.querySelector("#player");
+        const pcSb = document.querySelector("#computer");
+
+        const resetScore = document.querySelector("#reset");
+        resetScore.addEventListener("click", () => {
+            userScore = 0;
+            pcScore = 0;
+            playerSb.innerHTML = userScore;
+            pcSb.innerHTML = pcScore;
+        })
+
+        const changeScore = document.querySelector("#playing");
+        changeScore.addEventListener("click", () => {
+            playerSb.innerHTML = userScore;
+            pcSb.innerHTML = pcScore;
+        })
+
+        const rock = document.querySelector("#rock");
+        rock.addEventListener("click", () => {
+            let result = playRound("rock", computerPlay());
             if (result.slice(4,7) == "win") {
                 userScore++;
+                rock.classList.add("won");
+                rock.addEventListener("transitionend", () => {
+                    rock.classList.remove("won");
+                });
             }
             else if (result.slice(4,8) == "lose"){
                 pcScore++;
-            }
-
-
-            if(pcScore == userScore) {
-                console.log(`Tie! You: ${userScore} - PC: ${pcScore}`);
-            }
-            else if (pcScore > userScore) {
-                console.log(`You win! You: ${userScore} - PC: ${pcScore}`);
+                rock.classList.add("lost");
+                rock.addEventListener("transitionend", () => {
+                    rock.classList.remove("lost");
+                });
             }
             else {
-                console.log(`You lose! You: ${userScore} - PC: ${pcScore}`);
+                rock.classList.add("tie");
+                rock.addEventListener("transitionend", () => {
+                    rock.classList.remove("tie");
+                });
             }
-        }
+        });
 
-        game();
+        const paper = document.querySelector("#paper");
+        paper.addEventListener("click", () => {
+            let result = playRound("paper", computerPlay());
+            if (result.slice(4,7) == "win") {
+                userScore++;
+                paper.classList.add("won");
+                paper.addEventListener("transitionend", () => {
+                    paper.classList.remove("won");
+                });
+            }
+            else if (result.slice(4,8) == "lose"){
+                pcScore++;
+                paper.classList.add("lost");
+                paper.addEventListener("transitionend", () => {
+                    paper.classList.remove("lost");
+                });
+            }
+            else {
+                paper.classList.add("tie");
+                paper.addEventListener("transitionend", () => {
+                    paper.classList.remove("tie");
+                });
+            }
+        });
+
+        const scissors = document.querySelector("#scissors");
+        scissors.addEventListener("click", () => {
+            let result = playRound("scissors", computerPlay());
+            if (result.slice(4,7) == "win") {
+                userScore++;
+                scissors.classList.add("won");
+                scissors.addEventListener("transitionend", () => {
+                    scissors.classList.remove("won");
+                });
+            }
+            else if (result.slice(4,8) == "lose"){
+                pcScore++;
+                scissors.classList.add("lost");
+                scissors.addEventListener("transitionend", () => {
+                    scissors.classList.remove("lost");
+                });
+            }
+            else {
+                scissors.classList.add("tie");
+                scissors.addEventListener("transitionend", () => {
+                    scissors.classList.remove("tie");
+                });
+            }
+        });
+
+
+       
+
+        
+
+        
+
+
+        
